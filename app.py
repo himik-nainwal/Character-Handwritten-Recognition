8051ef00dad21ed527343efa90310b1973d520b1
himik-nainwal <59734430+himik-nainwal@users.noreply.github.com>
2022-07-02 21:44:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -62,23 +62,19 @@ if uploaded_file is not None:
      st.image(image, caption='Input Image')
      img_path = os.path.join("./data/test", uploaded_file.name)
      img_array =cv2.imread(img_path)
+     alpha_dict = {0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',7:'H',8:'I',9:'J',10:'K',11:'L',12:'M',13:'N',14:'O',15:'P',16:'Q',17:'R',18:'S',19:'T',20:'U',21:'V',22:'W',23:'X', 24:'Y',25:'Z'}
+     gray = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
+     gray = cv2.medianBlur(gray, 5)
+     ret, gray = cv2.threshold(gray, 75, 180, cv2.THRESH_BINARY)
+     element = cv2.getStructuringElement(cv2.MORPH_RECT, (90, 90))
+     gray = cv2.morphologyEx(gray, cv2.MORPH_GRADIENT, element)
+     gray = gray / 255.  # downsampling
+     gray = cv2.resize(gray, (28, 28))  # resizing
+     # reshaping the image
+     gray = np.reshape(gray, (28, 28))
 
-
-alpha_dict = {0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',7:'H',8:'I',9:'J',10:'K',11:'L',12:'M',13:'N',14:'O',15:'P',16:'Q',17:'R',18:'S',19:'T',20:'U',21:'V',22:'W',23:'X', 24:'Y',25:'Z'}
-
-
-gray = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
-gray = cv2.medianBlur(gray, 5)
-ret, gray = cv2.threshold(gray, 75, 180, cv2.THRESH_BINARY)
-element = cv2.getStructuringElement(cv2.MORPH_RECT, (90, 90))
-gray = cv2.morphologyEx(gray, cv2.MORPH_GRADIENT, element)
-gray = gray / 255.  # downsampling
-gray = cv2.resize(gray, (28, 28))  # resizing
-# reshaping the image
-gray = np.reshape(gray, (28, 28))
-
-#--> Displayong Result
-pred = alpha_dict[np.argmax(model.predict(np.reshape(gray, (1, 28, 28, 1))))]
-st.subheader('')
-st.text('Predicted as  - > ')
-st.write(pred)
+    #--> Displayong Result
+     pred = alpha_dict[np.argmax(model.predict(np.reshape(gray, (1, 28, 28, 1))))]
+     st.subheader('')
+     st.text('Predicted as  - > ')
+     st.write(pred)
